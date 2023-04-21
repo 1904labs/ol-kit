159e5ecc487c1d702e8f70821c8a7a5dc8d464e7
Noah Tatko <tatkon@rbtrainers.com>
2023-04-21 12:42:37 -0500

    get the map one working, with a few props

--- src/Map/Map.stories.js
+++ src/Map/Map.stories.js
@@ -5,10 +5,43 @@ export default {
   title: 'Example/Map',
   component: Map,
   tags: ['autodocs'],
+  argTypes: {
+    isMiltiMap: {
+      control: {
+        type: 'boolean',
+        default: false,
+      },
+    },
+    logoPosition: {
+      control: {
+        type: 'select',
+        options: ['left', 'right', 'none'],
+        default: 'none',
+      },
+    },
+    fullscreen: {
+      control: {
+        type: 'boolean',
+        default: false,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: '100%', height: '500px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
+  args: {
+    isMiltiMap: false,
+    logoPosition: 'none',
+    fullscreen: false,
+  },
 };
 
 // export const Secondary = {
